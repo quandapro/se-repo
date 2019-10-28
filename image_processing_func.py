93cb0887e10c1038ff27316e410aa34730e9fbbf
KkEditor <32573671+KkEditor@users.noreply.github.com>
2019-10-28 17:44:53 +0700

    divided into smaller branches for easier usage
    #dmkiwon

--- image_processing_func.py
+++ image_processing_func.py
@@ -2,103 +2,19 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def to_frequency_domain(img):
-    f = np.fft.fft2(img)
-    fshift = np.fft.fftshift(f)
-    magnitude_spectrum = 20 * np.log(np.abs(fshift))
-    return magnitude_spectrum
+#this file is only for testing.
 
-
-def high_pass_filter(img):
-    #use to detect edges of picture
-    f = np.fft.fft2(img)
-    fshift = np.fft.fftshift(f)
-    rows, cols = img.shape
-    crow, ccol = int(rows / 2), int(cols / 2)
-    fw_size=100
-    fshift[crow - int(fw_size/2):crow + int(fw_size/2), ccol - int(fw_size/2):ccol + int(fw_size/2)] = 0
-    f_ishift = np.fft.ifftshift(fshift)
-    img_back = np.fft.ifft2(f_ishift)
-    img_back = np.abs(img_back)
-    return img_back
-def gauss_blur(img,ksize):
-    #ksize needs to be odd number
-    res=cv2.GaussianBlur(img,(ksize,ksize),sigmaX=0)
-    return res
-
-def log_transform(img):
-    c = 255 / (np.log(1 + np.max(img)))
-    log_transformed = c * np.log(1 + img)
-    log_transformed = np.array(log_transformed, dtype=np.uint8)
-    return log_transformed
-
-def gamma_transform(img,gamma):
-    #gamma should be between 0.1 and 3
-    #gamma=1 is original picture
-    gamma_corrected = np.array(255 * (img / 255) ** gamma, dtype='uint8')
-    return gamma_corrected
-
-
-def down_sampling(img,factor):
-    #reduce number of pixels in image
-    height=img.shape[0]
-    length=img.shape[1]
-    res = cv2.resize(img, (int(length /factor), int(height / factor)))
-    res = cv2.resize(res, (length, height))
-    return res
-
-def to_negative(img):
-    res=cv2.bitwise_not(img)
-    return res
-
-def intensity_slicing(img,min_range,max_range):
-    #only map pixels in range, else is mapped 0
-    height = img.shape[0]
-    length = img.shape[1]
-    for i in range(height):
-        for j in range(length):
-            if img[i, j] > min_range and img[i, j] < max_range:
-                img[i, j] = 255
-            else:
-                img[i, j] = 0
-    return img
-
-def bit_plane_slicing(img,bit):
-    #bit should be between 0 and 7
-    plane = np.full((img.shape[0], img.shape[1]), 2 ** bit, np.uint8)
-    res = cv2.bitwise_and(plane, img)
-    x = res * 255
-    return x
-
-def optimize_preprocess(img):
-    #optimize picture before processing
-    rows, cols = img.shape
-    nrows = cv2.getOptimalDFTSize(rows)
-    ncols = cv2.getOptimalDFTSize(cols)
-    nimg=cv2.resize(img,(ncols,nrows))
-    return nimg
 
 def test(img):
-    height = img.shape[0]
-    length = img.shape[1]
-    for i in range(1,height):
-        for j in range(1,length):
-            if j%2==0:
-                img[i][j]=img[i][j-1]
-            if i%2==0:
-                img[i][j]=img[i-1][j]
-    return img
-
+    pass
 
 img = cv2.imread('1.jpg', 0)
 use_img = cv2.resize(img,(800,450))
-use_img=optimize_preprocess(use_img)
-
-
+# use_img=optimize_preprocess(use_img)
 cv2.imshow("original",use_img)
-#change function down here to test out all the functions
-res=bit_plane_slicing(use_img,2)
-cv2.imshow("edited",res)
+
+res=spatial_enchance(use_img)
+cv2.imshow("edited",res.astype('uint8'))
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
