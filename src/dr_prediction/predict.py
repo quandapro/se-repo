9323a93a2eb92d77d810b3789e6968565a3ba779
quandapro <quandapro@github.com>
2019-11-02 16:03:45 +0700

    Divide into folders

---
+++ src/dr_prediction/predict.py
@@ -0,0 +1,27 @@
+import os
+import tensorflow as tf 
+import keras
+from keras.applications import MobileNetV2
+from keras.layers import GlobalAveragePooling2D, Dense
+import cv2
+
+class Prediction:
+    def __init__(self, model_weights_path):
+        self.model = self.get_model(model_weights_path)
+    
+    def get_model(self, model_weights_path):
+        assert os.path.isfile(model_weights_path), "{} is not a file"
+        base_model = MobileNetV2(input_shape=(256, 256, 3), include_top=False)
+        x = GlobalAveragePooling2D()(base_model.output)
+        x = Dense(1, activation='sigmoid')(x)
+        model = Model(inputs=base_model.input, outputs=x)
+
+        model.load_weights(model_weights_path)
+
+        return model
+
+    def predict(self, image):
+        image = cv2.resize(image, (256, 256))
+        pred = model.predict(np.array([image]))
+        return pred
+        
---
+++ src/image_processing/image_recovery.py
@@ -0,0 +1,5 @@
+import cv2
+import numpy as np
+#nothing in here yet
+def mean_shift(img):
+    pass
