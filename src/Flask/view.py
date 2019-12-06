b6cddc416e7792dd2e05a4ffc18a718c0ecd47bd
KkEditor <32573671+KkEditor@users.noreply.github.com>
2019-12-07 05:41:11 +0700

    update Flask

--- src/Flask/view.py
+++ src/Flask/view.py
@@ -4,13 +4,9 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return render_template("index.html")
-
+@app.route('/signin')
 @app.route("/about")
-def about():
-    return """
-    <h1 style='color: red;'>I'm a red H1 heading!</h1>
-    <p>This is a lovely little paragraph</p>
-    <code>Flask is <em>awesome</em></code>
-    """
+@app.route('/service')
+@app.route("/")
 if __name__ == "__main__":
     app.run(port= 8080)
