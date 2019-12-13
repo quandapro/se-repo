320b200f2ee34f8a8d92fb3d927639756befef22
Quan Duong <quandapro@gmail.com>
2019-12-13 16:41:57 +0700

    Add login

--- src/Flask/view.py
+++ src/Flask/view.py
@@ -1,12 +1,63 @@
-from flask import Flask,render_template
+from flask import Flask, render_template, request, redirect, url_for, session
+from flask_mysqldb import MySQL
+import MySQLdb.cursors
+import re
+
 app = Flask(__name__)
+app.secret_key = 'thisissecret'
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'retinadb'
+
+mysql = MySQL(app)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
-@app.route('/signin')
-@app.route("/about")
-@app.route('/service')
-@app.route("/")
+    if 'loggedin' not in session:
+        return redirect(url_for('login'))
+    return redirect(url_for('home'))
+
+# Login 
+@app.route('/login/', methods=['GET', 'POST'])
+def login():
+    msg = ''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM users WHERE username = %s AND password = %s', (username, password))
+
+        account = cursor.fetchone()
+        print("account")
+
+        if account:
+            session['loggedin'] = True
+            session['username'] = account['username']
+
+            return redirect(url_for('home'))
+        else:
+            msg = 'Incorrect username/password!'
+
+    return render_template('login.html', msg=msg)
+
+# Logout
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+# Home
+@app.route('/home')
+def home():
+    if 'loggedin' not in session:
+        return redirect(url_for('login'))
+    return render_template('home.html', username=session['username'])
+# @app.route("/about")
+# @app.route('/service')
+
 if __name__ == "__main__":
     app.run(port= 8080)
