280110b538aeab7ce2fd58cdc94e6a1a53ba1925
quandapro <quandapro@github.com>
2019-12-22 17:50:43 +0700

    Fix admin route and views

--- src/Flask/view.py
+++ src/Flask/view.py
@@ -13,16 +13,21 @@ app.config['MYSQL_DB'] = 'retinadb'
 
 mysql = MySQL(app)
 
+@app.route("/index")
 @app.route("/")
 def index():
     if 'loggedin' not in session:
         return redirect(url_for('login'))
-    return redirect(url_for('home'))
+    if session['username'] == 'admin':
+        return redirect(url_for('admin'))
+    return redirect(url_for('doctor'))
 
 # Login 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     msg = ''
+    if 'loggedin' in session:
+        return redirect(url_for('index'))
     if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
         username = request.form['username']
         password = request.form['password']
@@ -36,8 +41,10 @@ def login():
         if account:
             session['loggedin'] = True
             session['username'] = account['username']
-
-            return redirect(url_for('home'))
+            if session['username'] == 'admin':
+                return redirect(url_for('admin'))
+            else:
+                return redirect(url_for('doctor'))
         else:
             msg = 'Incorrect username/password!'
 
@@ -50,44 +57,32 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
 
-# Home
-@app.route('/home')
-def home():
-    if 'loggedin' not in session:
-        return redirect(url_for('login'))
-    if session['username'] == 'admin':
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM users')
-        accounts = cursor.fetchall()
-        print(accounts)
-    return render_template('home.html', username=session['username'], accounts=accounts, len = len(accounts))
-
-@app.route('/admin', methods=['POST'])
-def add_doctor():
+@app.route('/admin', methods=['GET', 'POST'])
+def admin():
     msg = ''
     if 'username' not in session or session['username'] != 'admin':
         msg = 'You are not admin!'
         return redirect(url_for('login'))
-    else:
-        # ADD DOCTOR
-        if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
-            username = request.form['username']
-            password = request.form['password']
+    cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+    cursor.execute('SELECT * FROM users')
+    accounts = cursor.fetchall()
+    # ADD DOCTOR
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form:
+        username = request.form['username']
+        password = request.form['password']
 
-            cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-            cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
-            account = cursor.fetchone()
-            if account:
-                msg = 'Account already exists!'
-            else :
-                cursor.execute('INSERT INTO users VALUES (%s, %s)', (username, password))
-                mysql.connection.commit()
-                msg = 'You have successfully registered!'
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        if account:
+            msg = 'Account already exists!'
+        else :
+            cursor.execute('INSERT INTO users VALUES (%s, %s)', (username, password))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+            return redirect(url_for('admin', msg=msg))
 
-    print(msg)
-    return redirect(url_for('home'))
-
-
+    return render_template('admin.html', username=session['username'], accounts=accounts, len = len(accounts), msg=msg)
 
 if __name__ == "__main__":
     app.run(port= 8080)
