b9549b9c79b176ba518e578374ca2a1dcfb1d931
quandapro <quandapro@github.com>
2020-01-02 00:19:11 +0700

    Change db structure

--- retinadb.sql
+++ retinadb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 29, 2019 at 03:43 AM
+-- Generation Time: Jan 01, 2020 at 06:18 PM
 -- Server version: 10.4.10-MariaDB
 -- PHP Version: 7.3.12
 
@@ -25,22 +25,22 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `retina_image`
+-- Table structure for table `patient`
 --
 
-CREATE TABLE `retina_image` (
-  `patientID` int(11) NOT NULL,
-  `image_path` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `diagnosis` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
-  `diagnosed_by` varchar(100) COLLATE utf8_unicode_ci NOT NULL
+CREATE TABLE `patient` (
+  `patientID` varchar(20) COLLATE utf8_unicode_ci NOT NULL,
+  `diagnosis` text COLLATE utf8_unicode_ci NOT NULL,
+  `diagnosis_by` varchar(20) COLLATE utf8_unicode_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Dumping data for table `retina_image`
+-- Dumping data for table `patient`
 --
 
-INSERT INTO `retina_image` (`patientID`, `image_path`, `diagnosis`, `diagnosed_by`) VALUES
-(1, 'dummy', 'dummy', 'admin');
+INSERT INTO `patient` (`patientID`, `diagnosis`, `diagnosis_by`) VALUES
+('001639a390f0', 'DB lv 4', 'nguyenvana'),
+('0e3572b5884a', 'DB lv 4', 'nguyenvana');
 
 -- --------------------------------------------------------
 
@@ -66,37 +66,16 @@ INSERT INTO `users` (`username`, `password`) VALUES
 --
 
 --
--- Indexes for table `retina_image`
+-- Indexes for table `patient`
 --
-ALTER TABLE `retina_image`
-  ADD UNIQUE KEY `patientID` (`patientID`),
-  ADD KEY `diagnosed_by` (`diagnosed_by`);
+ALTER TABLE `patient`
+  ADD PRIMARY KEY (`patientID`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`username`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `retina_image`
---
-ALTER TABLE `retina_image`
-  MODIFY `patientID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `retina_image`
---
-ALTER TABLE `retina_image`
-  ADD CONSTRAINT `retina_image_ibfk_1` FOREIGN KEY (`diagnosed_by`) REFERENCES `users` (`username`) ON DELETE NO ACTION ON UPDATE NO ACTION;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
